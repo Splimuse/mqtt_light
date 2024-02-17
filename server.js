744d5cc1eef5c7f6dfa0fa7a25b2f69a1fbb7eb5
Splimuse <qwwqwqqw@rambler.ru>
2024-02-17 13:57:54 +0300

    delete unused comments

--- server.js
+++ server.js
@@ -40,8 +40,6 @@ mqttClient.on('connect', function () {
     mqttClient.subscribe(arr);
 });
 
-//webSocketServer.listen(3000, () => console.log("Server started"))
-
 let arrConn = []
 
 mqttClient.on('message', (topic, message) => {
@@ -56,7 +54,6 @@ mqttClient.on('message', (topic, message) => {
 webSocketServer.on('connection', webSocketConnection => {
     console.log(`client ${webSocketConnection} connected`)
     arrConn.push(webSocketConnection)
-    // console.log(`current state: ${[...state.entries()]}`)
     state.forEach((value, key) => {
         let stateEvent = key + ' - ' + value.toString();
         console.log(`state event: ${stateEvent}`)
@@ -69,59 +66,12 @@ webSocketServer.on('connection', webSocketConnection => {
         arr[1] = JSON.parse(arr[1]);
         arr[1] = Number(!arr[1]);
 
-        // console.log(arr[0]);
-
         mqttWrite(arr[0], arr[1])
     });
-    // webSocketConnection.send('Добро пожаловать на сервер');
 })
 
 function mqttWrite(topic, value) {
-    // const client2 = mqtt.connect(`mqtt://${ipAddr}:1883`);
-    // client2.on('connect', () => {
-
-    //     client2.subscribe(topic);
-
-    // })
-
-    // mqttClient.on('message', () => {
-    //     console.log(topic + 'and' + value);
-    //     mqttClient.publish(topic + '/on', String(value));
-
-    //     mqttClient.end();
-
-    // });
-
     console.log(topic + ' and ' + value);
     mqttClient.publish(topic + '/on', String(value));
 
 }
-
-function mqttRead(topic) {
-    mqttClient.publish(topic)
-}
-
-// webSocketServer.on('connection', ws => {
-//     ws.on('message', message => {
-//         webSocketServer.clients.forEach(client => {
-//             if (client.readyState === WebS.OPEN) {
-//                 let value = (Buffer.from(message)).toString();
-//                 let arr = value.split(',');
-//                 arr[1] = JSON.parse(arr[1]);
-//                 arr[1] = Number(!arr[1]);
-//                 let obj = {
-//                     topic: arr[0],
-//                     value: arr[1]
-//                 };
-
-//                 console.log(arr[0]);
-
-//                 mqttFunc(arr[0], arr[1])
-
-
-
-//             }
-//         });
-//     });
-//     ws.send('Добро пожаловать на сервер');
-// });
