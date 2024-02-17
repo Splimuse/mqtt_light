13c087dc96484b4d5ffe4bdb884a2b5c7d151eb2
Splimuse <qwwqwqqw@rambler.ru>
2024-02-17 12:49:32 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const button = document.querySelectorAll('button');
+
+const ws = new WebSocket('ws://localhost:3000');
+
+function setStatus(value) {
+    console.log(value);
+}
+
+
+
+button.forEach(function (btn) {
+    btn.addEventListener('click', event => {
+
+        console.log(btn.id);
+
+        ws.send([btn.id, btn.value]);
+
+    });
+
+})
+
+ws.onopen = () => setStatus('ONLINE');
+
+ws.onclose = () => setStatus('DISCONNECTED');
+
+ws.onmessage = (msg) => {
+    let res = msg.data.split(' - ');
+    console.log(res[0]);
+    console.log(res[1]);
+
+    document.getElementById(res[0]).value = res[1];
+    document.getElementById(res[0]).textContent = res[1];
+    if (Number(res[1]) == 0) {
+        document.getElementById(res[0]).style.backgroundColor = "silver";
+    } else {
+        document.getElementById(res[0]).style.backgroundColor = "yellow";
+    }
+    console.log(res);
+
+};
